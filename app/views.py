7532e7f9223820ee400f3b3047f6cf6c0732505b
juwaini <juwaini@gmail.com>
2017-02-18 23:47:32 +0800

    Solve problem 2

--- app/views.py
+++ app/views.py
@@ -6,4 +6,12 @@ def get_name_and_company():
 
     for employee in employees:
         print("Name: %s, Company: %s" % (employee.name, employee.company.name))
-        
+
+
+def get_employee_name_older_than_50():
+    employees = Employee.objects.filter(age__gt=50)
+
+    for employee in employees:
+        print('Name: %s' % employee.name)
+
+
